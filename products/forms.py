1815520a0582736df29d0971fdfd2f395c2bc906
VaibhubCodes <167017011+VaibhubCodes@users.noreply.github.com>
2025-05-08 13:31:43 +0530

    Delete products directory

--- products/forms.py
+++
@@ -1,8 +0,0 @@
-# products/forms.py
-
-from django import forms
-
-class SubCategoryCSVUploadForm(forms.Form):
-    csv_file = forms.FileField(label='Upload CSV File')
-class ProductCSVUploadForm(forms.Form):
-    csv_file = forms.FileField(label='Upload Product CSV File')
